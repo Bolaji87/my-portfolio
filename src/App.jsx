f61ef257470a3f8ae8406741eaa021a2de8636ff
bolaji87 <sakabolaji77@gmai.com>
2024-08-11 12:26:04 -0700

    add contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
