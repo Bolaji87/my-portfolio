f2426a83fa97a2addb389068b50e5d5e3ccdeb8c
bolaji87 <sakabolaji77@gmai.com>
2024-08-11 12:33:15 -0700

    initial commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+function Projects() {
+  return (
+    <>
+      <div id="projects">
+        <h1>My work so far</h1>
+        <div className="works-done">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <hr className="custom-hr" />
+    </>
+  );
+}
+
+export default Projects;
